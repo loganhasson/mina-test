a0be1b481e8b7e421c313b005a56bd4d3c609189
Logan Hasson <logan.hasson@gmail.com>
2014-07-07 20:00:04 -0400

    Setup unicorn config

---
+++ config/unicorn.rb
@@ -0,0 +1,10 @@
+working_directory "/home/root/mina-test"
+
+pid "/home/root/mina-test/pids/unicorn.pid"
+
+stderr_path "/home/root/mina-test/log/unicorn.log"
+stdout_path "/home/root/mina-test/log/unicorn.log"
+
+worker_processes 2
+
+timeout 30
