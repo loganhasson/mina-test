e4d7a93b42dbb2508ccb13977a3e29e2febafd24
Logan Hasson <logan.hasson@gmail.com>
2014-07-07 20:51:30 -0400

    Update unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 working_directory "/home/root/mina-test/current"
 
-pid "/home/root/mina-test/pids/unicorn.pid"
+pid "/home/root/mina-test/tmp/pids/unicorn.pid"
 
 stderr_path "/home/root/mina-test/log/unicorn.log"
 stdout_path "/home/root/mina-test/log/unicorn.log"
